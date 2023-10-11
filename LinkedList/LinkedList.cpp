70993688c81b1f405fe5204d9b1854a4569f9460
Tekkers75 <Tekkers75@gmail.com>
2023-10-11 16:07:55 +0900

    Добавьте файлы проекта.

---
+++ LinkedList/LinkedList.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include "LinkedList.h"
+
+
+int main() {
+
+    setlocale(LC_ALL, "rus");
+    LinkedList list;
+
+    list.insert(30);
+    list.insert(10);
+    list.insert(50);
+    list.insert(20);
+    list.insert(40);
+
+    // Поиск элемента
+    int searchData = 30;
+    Node* foundNode = list.search(searchData);
+    if (foundNode != nullptr) {
+        std::cout << "Элемент " << searchData << " найден." << std::endl;
+    }
+    else {
+        std::cout << "Элемент " << searchData << " не найден." << std::endl;
+    }
+
+    list.print();
+
+    int deleteData = 30;
+    list.remove(deleteData);
+
+    list.print();
+
+    std::cout << "Исходный список: ";
+    list.print();
+
+    list.sort();
+
+    std::cout << "Отсортированный список: ";
+    list.print();
+     
+
+    return 0;
+}
+
---
+++ LinkedList/LinkedList.h
@@ -0,0 +1,121 @@
+#pragma once
+#include <iostream>
+
+class Node{
+public:
+    int data;
+    Node* next;
+    Node* prev;
+
+    Node(int data) {
+        this->data = data;
+        next = nullptr;
+        prev = nullptr;
+    }
+};
+
+
+class LinkedList {
+private:
+    Node* head;
+    Node* tail;
+
+public:
+    LinkedList() {
+        head = nullptr;
+        tail = nullptr;
+    }
+
+    void insert(int data) {
+        Node* newNode = new Node(data);
+        // Если список пустой, новый узел становится головным и хвостовым
+        if (head == nullptr) {
+            head = newNode;
+            tail = newNode;
+        }
+        else {
+            // Привязываем новый узел к текущему хвосту
+            tail->next = newNode;
+            // Устанавливаем ссылку на предыдущий узел
+            newNode->prev = tail;
+            // Новый узел становится новым хвостом
+            tail = newNode;
+        }
+    }
+
+    void print() {
+        Node* current = head;
+        while (current != nullptr) {
+            // Выводим данные текущего узла
+            std::cout << current->data << " ";
+            // Переходим к следующему узлу
+            current = current->next;
+        }
+        std::cout << std::endl;
+    }
+
+    Node* search(int data) {
+        Node* current = head;
+        while (current != nullptr) {
+            if (current->data == data) {
+                // Возвращаем узел, если найден элемент
+                return current;  
+            }
+            current = current->next;
+        }
+        // Если элемент не найден, возвращаем nullptr
+        return nullptr; 
+    }
+
+    void remove(int data) {
+        Node* current = head;
+        while (current != nullptr) {
+            if (current->data == data) {
+                if (current == head) {
+                    // Обновляем голову списка, если удаляемый элемент - головной узел
+                    head = current->next;  
+                }
+                if (current == tail) {
+                    // Обновляем хвост списка, если удаляемый элемент - хвостовой узел
+                    tail = current->prev;  
+                }
+                if (current->prev != nullptr) {
+                    // Обновляем ссылку на следующий узел для предыдущего узла
+                    current->prev->next = current->next;  
+                }
+                if (current->next != nullptr) {
+                    // Обновляем ссылку на предыдущий узел для следующего узла
+                    current->next->prev = current->prev;  
+                }
+                // Удаляем узел
+                delete current;  
+                return;
+            }
+            current = current->next;
+        }
+    }
+
+    // Сортировка выбором
+    void sort() {
+        Node* current = head;
+        while (current != nullptr) {
+            Node* minNode = current;
+            Node* temp = current->next;
+            while (temp != nullptr) {
+                if (temp->data < minNode->data) {
+                    minNode = temp;
+                }
+                temp = temp->next;
+            }
+            if (minNode != current) {
+                // Обмен значениями узлов
+                int tempData = current->data;
+                current->data = minNode->data;
+                minNode->data = tempData;
+            }
+            current = current->next;
+        }
+    }
+
+};
+
